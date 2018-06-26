ab01b12b84876062908e6fef0c81840f89400d7a
Tatiana Korsakova <tatiana.korsakov@gmail.com>
2018-06-26 11:06:01 +0300

    подключила js

--- source/js/popup.js
+++ source/js/popup.js
@@ -11,24 +11,6 @@ var text = popupletter.querySelector("[name=text]");
 var storage = localStorage.getItem("login");
 var isStorageSupport = true;
 var storage = "";
-var cart10 = document.querySelector(".product__item--index:nth-child(1) .button--product-buy");
-var popupcart = document.querySelector(".modal--cart");
-var closecart = document.querySelector(".modal__button-close--form");
-
-
-/*
-var cart1 = document.querySelector(".product__item:nth-child(1) .button--product-buy");
-var cart2 = document.querySelector(".product__item:nth-child(2) .button--product-buy");
-var cart3 = document.querySelector(".product__item:nth-child(3) .button--product-buy");
-var cart4 = document.querySelector(".product__item:nth-child(4) .button--product-buy");
-var cart5 = document.querySelector(".product__item:nth-child(5) .button--product-buy");
-var cart6 = document.querySelector(".product__item:nth-child(6) .button--product-buy");
-var cart7 = document.querySelector(".product__item:nth-child(7) .button--product-buy");
-var cart8 = document.querySelector(".product__item:nth-child(8) .button--product-buy");
-var cart9 = document.querySelector(".product__item:nth-child(9) .button--product-buy");
-var popupcart = document.querySelector(".modal--cart");
-var closecart = document.querySelector(".modal__button-close--form");
-*/
 
 letter.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -93,71 +75,6 @@ window.addEventListener("keydown", function (evt) {
     evt.preventDefault();
     if (popupletter.classList.contains("modal--show")) {
       popupletter.classList.remove("modal--show");
-    }
-  }
-});
-
-/*
-cart1.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupcart.classList.add("modal--show");
-});
-
-cart2.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupcart.classList.add("modal--show");
-});
-
-cart3.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupcart.classList.add("modal--show");
-});
-
-cart4.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupcart.classList.add("modal--show");
-});
-
-cart5.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupcart.classList.add("modal--show");
-});
-
-cart6.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupcart.classList.add("modal--show");
-});
-
-cart7.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupcart.classList.add("modal--show");
-});
-
-cart8.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupcart.classList.add("modal--show");
-});
-
-cart9.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupcart.classList.add("modal--show");
-});*/
-
-cart10.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupcart.classList.add("modal--show");
-
-
-closecart.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popupcart.classList.remove("modal--show");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (popupcart.classList.contains("modal--show")) {
-      popupcart.classList.remove("modal--show");
     }
   }
 });
